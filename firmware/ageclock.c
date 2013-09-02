70fdeef05d03a9e215644abebcbc52cbc299be0a
Rex Feany <laeos@laeos.net>
2013-09-02 14:57:46 -0700

    better time calculations

--- firmware/ageclock.c
+++ firmware/ageclock.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
 #include <math.h>
 
 #include "util.h"
@@ -12,20 +13,17 @@
 extern volatile uint8_t time_s, time_m, time_h;
 extern volatile uint8_t date_m, date_d, date_y;
 
-struct date {
-    uint32_t year;
-    uint32_t month;
-    uint32_t day;
-    uint32_t hour;
-    uint32_t minute;
-    uint32_t seconds;
+struct sdate {
+    uint32_t y;
+    uint32_t m;
+    uint32_t d;
 };
 
 struct age {
     uint32_t years;
     uint32_t months;
-    uint32_t weeks;
     uint32_t days;
+
     uint32_t hours;
     uint32_t minutes;
     uint32_t seconds;
@@ -34,87 +32,95 @@ struct age {
 #include "demo.h"
 
 static struct age age;
+static uint32_t calstart;
 
-static int compare_mdhm(const struct date *a, const struct date *b)
+/* convert date to day number */
+static uint32_t gday(const struct sdate *d)
 {
-    int r;
-
-    if ((r = a->month - b->month))
-	return r;
-    if ((r = a->day - b->day))
-	return r;
-    if ((r = a->hour - b->hour))
-	return r;
-    if ((r = a->minute - b->minute))
-	return r;
-    return 0;
+    uint32_t  y, m;
+    m = (d->m + 9)%12;      /* mar=0, feb=11 */
+    y = d->y - m/10;        /* if Jan/Feb, year-- */
+    return y*365 + y/4 - y/100 + y/400 + (m*306 + 5)/10 + (d->d - 1);
 }
 
-static void so_far(const struct date *today, struct age *age)
+/* convert day number to y,m,d format */
+static void dtf(uint32_t od, struct sdate *pd)
 {
-    age->seconds += today->seconds;
-    age->minutes += today->minute;
-    age->hours += today->hour;
-    age->days += today->day - 1;
-    age->months += today->month - 1;
+    int64_t y, ddd, mi, d = od;
+
+    y = (10000UL*d + 14780UL)/3652425UL;
+    ddd = d - (y*365 + y/4 - y/100 + y/400);
+    if (ddd < 0) {
+        y--;
+        ddd = d - (y*365 + y/4 - y/100 + y/400);
+    }
+    mi = (52 + 100*ddd)/3060;
+    pd->y = y + (mi + 2)/12;
+    pd->m = (mi + 2)%12 + 1;
+    pd->d = ddd - (mi*306 + 5)/10 + 1;
 }
 
-static void diff(const struct date *birth, const struct date *today, struct age *age)
+static void setup_calc(void)
 {
-    memcpy(age, &start_age, sizeof(*age));
-    so_far(today, age);
-    age->years = today->year - (birth->year + 1);
-    if (compare_mdhm(birth, today) < 0)
-	age->years++;
-}
-
-static uint32_t boink(uint32_t *from, uint32_t count)
-{
-    uint32_t mod = *from % count;
-    uint32_t rok = (*from - mod) / count;
-    *from = mod;
-    return rok;
-}
-
-static void normalize(struct age *age)
-{
-    double f, m;
-
-    age->minutes += boink(&age->seconds, 60);
-    age->hours += boink(&age->minutes, 60);
-    age->days += boink(&age->hours, 24);
-    age->weeks += boink(&age->days, 7);
-
-    f = age->weeks;
-    m = truncf(f / 4.3);
-    age->months += m;
-    age->weeks -= truncf((m * 4.3));
-
-    age->years += boink(&age->months, 12);
-}
-
-void initanim(void)
-{
-}
-
-void initdisplay(uint8_t inverted)
-{
-    glcdClearScreen();
-}
-
-void step(void)
-{
-    struct date today = {
-	2000 + date_y,
-	date_m,
-	date_d,
-	time_h,
-	time_m,
-	time_s
+    struct sdate s = {
+        .y = 1582,
+        .m = 10,
+        .d =1
     };
-    diff(&the_birth, &today, &age);
-    normalize(&age);
+    calstart = gday(&s);
 }
+
+/* return gday, or exit if bad date */
+static bool legald(const struct sdate *d, uint32_t *v)
+{
+    struct sdate t;
+    uint32_t g;
+
+    g = gday(d);
+    if (g < calstart)
+	return false;
+
+    dtf(g, &t);
+    if (d->y == t.y && d->m == t.m && d->d == t.d) {
+	*v = g;
+	return true;
+    }
+
+    return false;
+}
+
+static uint32_t get_today(void)
+{
+    struct sdate today;
+
+    today.y = 2000 + date_y;
+    today.m = date_m;
+    today.d = date_d;
+
+    return gday(&today);
+}
+
+static void normalize(uint32_t days)
+{
+    age.years = days / 365;
+    days -= age.years * 365;
+
+    age.months = trunc(days / 30.42);
+    days -= trunc(age.months * 30.42);
+
+    age.days = days;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 #define NLINES 8
 #define NCOLS  21
@@ -169,14 +175,29 @@ static void lputs(const char *str)
 	lputc(*str++);
 }
 
-static void lputn(uint8_t n)
+
+static bool around(uint32_t digit, uint32_t *value, bool printed)
 {
-    if (n >= 100) {
-	lputc(n / 100 + '0');
-	n = n % 100;
+    if (*value >= digit) {
+	lputc(*value / digit + '0');
+	*value = *value % digit;
+	return true;
+    } else if (printed) {
+	lputc('0');
+	return true;
     }
-    if (n >= 10)
-	lputc(n / 10 + '0');
+    return false;
+}
+
+
+static void lputn(uint32_t n)
+{
+    bool did = false;
+    did = around(100000, &n, did);
+    did = around(10000, &n, did);
+    did = around(1000, &n, did);
+    did = around(100, &n, did);
+    did = around(10, &n, did);
     lputc(n % 10 + '0');
 }
 
@@ -204,6 +225,30 @@ static void lprint_dow(uint8_t mon, uint8_t day, uint8_t yr)
     lputs(dow_data[dotw(mon,day,yr)]);
 }
 
+
+void initanim(void)
+{
+    setup_calc();
+}
+
+void initdisplay(uint8_t inverted)
+{
+    glcdClearScreen();
+}
+
+void step(void)
+{
+    uint32_t diff, past, today = get_today();
+
+    if (legald(&the_birth, &past)) {
+	diff = (past > today) ? past - today : today - past;
+	normalize(diff);
+	age.hours = time_h;
+	age.minutes = time_m;
+	age.seconds = time_s;
+    } 
+}
+
 void draw(uint8_t inverted)
 {
     int did = 0;
@@ -228,17 +273,6 @@ void draw(uint8_t inverted)
 	lputs(" month");
 
 	if (age.months != 1)
-	    lputc('s');
-	lputs(", ");
-    }
-
-    if (did == 2) { next_line(); did = 0; }
-
-    if (age.weeks) {
-	did++;
-	lputn(age.weeks);
-	lputs(" week");
-	if (age.weeks != 1)
 	    lputc('s');
 	lputs(", ");
     }
@@ -287,8 +321,8 @@ void draw(uint8_t inverted)
 
     next_line();
 
-    if ((the_birth.month == date_m) &&
-	    (the_birth.day  == date_d))
+    if ((the_birth.m == date_m) &&
+	    (the_birth.d  == date_d))
 	lputs("   HAPPY BIRTHDAY!   ");
 
     last_line();
@@ -317,4 +351,5 @@ void setscore(void)
 void init_crand(void)
 {
 }
+
 #endif
